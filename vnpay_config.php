0cda7849f64671ea0f6563f2363796da9ac98ec3
cvh1204gt <chuvanhung1122004@gmail.com>
2025-05-13 11:20:59 +0700

    Cập nhật mới

---
+++ vnpay_config.php
@@ -0,0 +1,6 @@
+<?php
+$vnp_TmnCode = "1FDTFMEP"; // Mã website
+$vnp_HashSecret = "KNUF0U6XWU8Y1OPGQW292HFPXT1L75H5"; // Chuỗi bí mật
+$vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+$vnp_Returnurl = "http://yourdomain.com/vnpay_return.php"; // URL sau khi thanh toán xong
+?>
